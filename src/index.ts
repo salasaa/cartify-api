542236ea5318b6384fc5a8e59049140fe33c1ea4
salasaa <akhirudinsalasa.gc@gmail.com>
2025-09-01 21:55:07 +0700

    feat: implement initial endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type grocery = {
+  id: number;
+  name: string;
+  description: string;
+};
 
-export default app
+let groceries: grocery[] = [
+  { id: 1, name: "Rice", description: "staple food" },
+  { id: 2, name: "Egg", description: "easy protein sources" },
+  { id: 3, name: "Banana", description: "fruits containing antioxidants" },
+];
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Welcom to Cartify-API!",
+  });
+});
+
+app.get("/groceries", (c) => {
+  return c.json(groceries);
+});
+
+app.get("/groceries/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  const grocery = groceries.find((grocery) => {
+    return grocery.id === id;
+  });
+
+  if (!grocery) {
+    return c.json({ message: "Grocery not found" }, 404);
+  }
+
+  return c.json(grocery);
+});
+
+export default app;
