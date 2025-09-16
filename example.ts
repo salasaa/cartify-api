164bfffa378253a35cc0d00f5e67a96118614fd9
salasaa <akhirudinsalasa.gc@gmail.com>
2025-09-16 21:57:09 +0700

    feat: connecting db and add Prisma commands to package.json

--- example.ts
+++ example.ts
@@ -2,21 +2,22 @@ import * as pg from "pg";
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
-
-  // If using separate variables
-  // host: process.env.POSTGRES_HOST,
-  // user: process.env.POSTGRES_USER,
-  // password: process.env.POSTGRES_PASSWORD,
-  // database: process.env.POSTGRES_DB,
-  // port: Number(process.env.POSTGRES_PORT),
 });
-
 await client.connect();
 
-const res = await client.query("SELECT * FROM groceries");
+type Grocery = {
+  id: number;
+  name: string;
+  category: string;
+  description: string;
+};
 
-const groceries = res.rows;
-
-console.log({ groceries });
-
-await client.end();
+try {
+  const result = await client.query("SELECT * FROM groceries");
+  const groceries: Grocery[] = result.rows;
+  console.log({ groceries });
+} catch (error) {
+  console.error("Failed to connect to the database", error);
+} finally {
+  await client.end();
+}
