0077d5df6fe37e729db2adaaf356ef59d55f0053
salasaa <akhirudinsalasa.gc@gmail.com>
2025-09-18 13:51:15 +0700

    feat: setup & integrate Prisma for db operations

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,27 @@
 import { Hono } from "hono";
-import { dataGroceries } from "./data/groceries";
+import { db } from "./lib/db";
 
 const app = new Hono();
 
-let groceries = dataGroceries;
-
 app.get("/", (c) => {
   return c.json({
-    message: "Welcome to Cartify-API!",
+    title: "Cartify-API!",
   });
 });
 
-app.get("/groceries", (c) => {
+app.get("/groceries", async (c) => {
+  const groceries = await db.grocery.findMany();
+
   return c.json(groceries);
 });
 
-// GET grocery by category
-app.get("/groceries/category/:category", (c) => {
-  const category = c.req.param("category");
-
-  // return c.json(category);
-
-  const filteredGroceries = groceries.filter((grocery) =>
-    grocery.category.toLowerCase().includes(category.toLowerCase())
-  );
-
-  if (!filteredGroceries) {
-    return c.json(
-      {
-        message: "No category found",
-        data: null,
-      },
-      404
-    );
-  }
-
-  return c.json(filteredGroceries);
-});
-
-// GET grocery item by ID
-app.get("/groceries/:id", (c) => {
+app.get("/groceries/:id", async (c) => {
   const id = Number(c.req.param("id"));
+  const grocery = await db.grocery.findUnique({ where: { id } });
 
-  const grocery = groceries.find((grocery) => {
-    return grocery.id === id;
-  });
-
-  if (!grocery) {
-    return c.json({ message: "Grocery not found" }, 404);
-  }
+  if (!grocery) return c.notFound();
 
   return c.json(grocery);
-});
-
-// POST a new grocery
-app.post("/groceries", async (c) => {
-  const body = await c.req.json();
-
-  const newGrocery = {
-    id: groceries[groceries.length - 1]?.id + 1 || 1,
-    ...body,
-  };
-  groceries = [newGrocery.id, ...groceries, newGrocery];
-
-  return c.json(
-    {
-      message: "Grocery added successfully",
-      data: newGrocery,
-    },
-    201
-  );
 });
 
 // DELETE all groceries
