502a76e8a0b1ef5d57cc1e4dcba931dd2d8e1778
salasaa <akhirudinsalasa.gc@gmail.com>
2025-09-25 14:41:31 +0700

    feat: migrate category slug

---
+++ prisma/migrations/20250923165307_updated_at/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."Category" ALTER COLUMN "updatedAt" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "public"."Grocery" ALTER COLUMN "updatedAt" DROP DEFAULT;
---
+++ prisma/migrations/20250923174406_category_slug/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Grocery" ADD COLUMN     "categorySlug" TEXT;
