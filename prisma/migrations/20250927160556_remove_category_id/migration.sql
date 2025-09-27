d3e948a91573aeed75338d8121afb473fc7e8533
salasaa <akhirudinsalasa.gc@gmail.com>
2025-09-27 23:06:46 +0700

    fix: remove categoryId from Grocery model

---
+++ prisma/migrations/20250927160556_remove_category_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `categoryId` on the `Grocery` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Grocery" DROP COLUMN "categoryId";
